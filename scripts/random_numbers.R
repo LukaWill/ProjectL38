c3d280c6933399e04327c5ada3d6c06c6a1407de
LukaWill <101551767+LukaWill@users.noreply.github.com>
2022-06-15 20:38:59 +0200

    Update random_numbers.R

--- scripts/random_numbers.R
+++ scripts/random_numbers.R
@@ -4,4 +4,4 @@ setwd("~/Project_bioinformatics")
 
 Random <-read.delim("DEGs.txt", header=F)
 All <- Random[sample(nrow(Random), 823), ]
-write.table(as.data.frame(All), "Random_823.xls", row.names=T, quote=FALSE, sep="\t")
+write.table(as.data.frame(All), "Random_823.txt", row.names=T, quote=FALSE, sep="\t")
