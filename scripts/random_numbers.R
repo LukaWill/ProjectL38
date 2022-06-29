c689f6a68dfa655bed2bca23415737a477a10b95
LukaWill <101551767+LukaWill@users.noreply.github.com>
2022-06-29 21:24:45 +0200

    Delete random_numbers.R

--- scripts/random_numbers.R
+++
@@ -1,7 +0,0 @@
-### Random picking of numbers
-
-setwd("~/Project_bioinformatics")
-
-Random <-read.delim("DEGs.txt", header=F)
-All <- Random[sample(nrow(Random), 823), ]
-write.table(as.data.frame(All), "Random_823.txt", row.names=T, quote=FALSE, sep="\t")
